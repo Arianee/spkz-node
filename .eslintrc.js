abc6ffcfdb44ed676419a2f44080ff95c3a086d7
mouadhbb <mouadh@arianee.org>
2021-09-24 09:53:10 +0200

    feat(project): Init project

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'jest'],
+  rules: {
+    'no-underscore-dangle': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'import/no-unresolved': 'error',
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import {MessageService} from './services/message.service';
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get(`${process.env.CONTEXT_PATH}/ping`, (req: any, res: any) => {
+  res.send('pong');
+});
+
+app.post(`${process.env.CONTEXT_PATH}/spkz/rpc`, (new MessageService().getMessagesJSONRPC()));
+
+app.listen(port, async () => {
+  console.info(`spkz-node listening at port:${port}`);
+
+});
