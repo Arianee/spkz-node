7b8e7dd479253589856839f6663a706f50cc3eb6
Maxime <maxime@vaugam.com>
2021-10-18 17:09:54 +0200

    fix(sentry): fix sentry push in ci

--- sentry.js
+++ sentry.js
@@ -1,8 +1,6 @@
 const { execSync } = require('child_process');
 const packageJSON = require('./package.json');
 
-execSync('npm i -g  @sentry/cli');
-
 const { version } = packageJSON;
 
 console.log(`starting uploading source map to sentry of version: ${version}`);
